73386dbac4db9f129110b34e0488a450c3dc7e97
Gaetan Semet <gaetan@xeberon.net>
2017-01-30 11:53:22 +0100

    Fix Travis build for Trusty Ubuntu
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- bootstrap-debian.sh
+++ bootstrap-debian.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+echo "Bootstraping required system packages for Guake (Debian/Ubuntu)"
+
 sudo apt-get install -q \
-    python-dbus \
-    python3-dbus \
-    python-gi \
-    python3-gi \
-    python-keybinder \
-    libkeybinder-3.0-0 \
+    gir1.2-gtk-3.0 \
     gir1.2-keybinder \
-    gir1.2-gtk-3.0
+    libgtk-3-0 \
+    libkeybinder-3.0-0 \
+    libvte-2.90-9 \
+    python-dbus \
+    python-gi \
+    python-gobject \
+    python-keybinder \
+    python3-dbus \
+    python3-gi \
